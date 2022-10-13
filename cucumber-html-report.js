2613f13d0fdcfb78cc892c79cee43619122782c1
valwillgithub <vastyjay@yahoo.com>
2022-10-13 01:04:03 +0100

    got cucumber report working

--- cucumber-html-report.js
+++ cucumber-html-report.js
@@ -1,16 +1,16 @@
-const report = require("multiple-cucumber-html-reporter");
+const report = require('multiple-cucumber-html-reporter');
 report.generate({
-  jsonDir: "jsonlogs", // ** Path of .json file **//
-  reportPath: "./reports/cucumber-htmlreport.html",
-  metadata: {
-    browser: {
-      name: "chrome",
-      version: "XX",
-    },
-    device: "Local test machine",
-    platform: {
-      name: "Windows",
-      version: "11",
-    },
-  },
+	jsonDir: 'jsonlogs', // ** Path of .json file **//
+	reportPath: './reports/cucumber-report',
+	metadata: {
+		browser: {
+			name: 'chrome',
+			version: 'XX',
+		},
+		device: 'Local test machine',
+		platform: {
+			name: 'macOS',
+			version: 'Big Sur',
+		},
+	},
 });
